54e48b17a7f7d78194b10f752235478e1556b6a3
Paul Alfredo <155785978+paulafredo@users.noreply.github.com>
2025-05-20 18:58:01 +0200

    Ajoutez des fichiers projet.

---
+++ Form/Form2.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace GuestExtractor
+{
+    partial class Form2
+    {
+        private System.ComponentModel.IContainer components = null;
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtInputDir = new System.Windows.Forms.TextBox();
+            this.txtOutputDir = new System.Windows.Forms.TextBox();
+            this.btnSelectInputDir = new System.Windows.Forms.Button();
+            this.btnSelectOutputDir = new System.Windows.Forms.Button();
+            this.btnDump = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.statusTextBox = new System.Windows.Forms.TextBox();
+            this.guna2AnimateWindow1 = new Guna.UI2.WinForms.Guna2AnimateWindow(this.components);
+            this.guna2BorderlessForm1 = new Guna.UI2.WinForms.Guna2BorderlessForm(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnResetGuest = new System.Windows.Forms.Button();
+            this.guna2AnimateWindow2 = new Guna.UI2.WinForms.Guna2AnimateWindow(this.components);
+            this.btnClear = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.guna2ControlBox1 = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.guna2ControlBox2 = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.guna2ContainerControl1 = new Guna.UI2.WinForms.Guna2ContainerControl();
+            this.guna2ContainerControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtInputDir
+            // 
+            this.txtInputDir.Location = new System.Drawing.Point(30, 115);
+            this.txtInputDir.Name = "txtInputDir";
+            this.txtInputDir.Size = new System.Drawing.Size(300, 20);
+            this.txtInputDir.TabIndex = 0;
+            // 
+            // txtOutputDir
+            // 
+            this.txtOutputDir.Location = new System.Drawing.Point(30, 161);
+            this.txtOutputDir.Name = "txtOutputDir";
+            this.txtOutputDir.Size = new System.Drawing.Size(300, 20);
+            this.txtOutputDir.TabIndex = 1;
+            // 
+            // btnSelectInputDir
+            // 
+            this.btnSelectInputDir.Location = new System.Drawing.Point(335, 115);
+            this.btnSelectInputDir.Name = "btnSelectInputDir";
+            this.btnSelectInputDir.Size = new System.Drawing.Size(75, 20);
+            this.btnSelectInputDir.TabIndex = 2;
+            this.btnSelectInputDir.Text = "...";
+            this.btnSelectInputDir.Click += new System.EventHandler(this.btnSelectInputDir_Click);
+            // 
+            // btnSelectOutputDir
+            // 
+            this.btnSelectOutputDir.Location = new System.Drawing.Point(336, 161);
+            this.btnSelectOutputDir.Name = "btnSelectOutputDir";
+            this.btnSelectOutputDir.Size = new System.Drawing.Size(75, 20);
+            this.btnSelectOutputDir.TabIndex = 3;
+            this.btnSelectOutputDir.Text = "...";
+            this.btnSelectOutputDir.Click += new System.EventHandler(this.btnSelectOutputDir_Click);
+            // 
+            // btnDump
+            // 
+            this.btnDump.Location = new System.Drawing.Point(30, 187);
+            this.btnDump.Name = "btnDump";
+            this.btnDump.Size = new System.Drawing.Size(75, 23);
+            this.btnDump.TabIndex = 4;
+            this.btnDump.Text = "DUMP";
+            this.btnDump.Click += new System.EventHandler(this.btnDump_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(111, 187);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 5;
+            this.btnReset.Text = "RESET";
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // statusTextBox
+            // 
+            this.statusTextBox.Location = new System.Drawing.Point(30, 262);
+            this.statusTextBox.Multiline = true;
+            this.statusTextBox.Name = "statusTextBox";
+            this.statusTextBox.ReadOnly = true;
+            this.statusTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.statusTextBox.Size = new System.Drawing.Size(380, 176);
+            this.statusTextBox.TabIndex = 6;
+            this.statusTextBox.TextChanged += new System.EventHandler(this.statusTextBox_TextChanged);
+            // 
+            // guna2BorderlessForm1
+            // 
+            this.guna2BorderlessForm1.ContainerControl = this;
+            this.guna2BorderlessForm1.DockIndicatorColor = System.Drawing.Color.IndianRed;
+            this.guna2BorderlessForm1.DockIndicatorTransparencyValue = 0.6D;
+            this.guna2BorderlessForm1.TransparentWhileDrag = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(153, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 21);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "GuestExtractor";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(27, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 41;
+            this.label2.Text = "Folder Output";
+            // 
+            // btnResetGuest
+            // 
+            this.btnResetGuest.Location = new System.Drawing.Point(30, 216);
+            this.btnResetGuest.Name = "btnResetGuest";
+            this.btnResetGuest.Size = new System.Drawing.Size(156, 23);
+            this.btnResetGuest.TabIndex = 43;
+            this.btnResetGuest.Text = "GUEST RESET UID";
+            this.btnResetGuest.Click += new System.EventHandler(this.btnResetGuest_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(336, 233);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 44;
+            this.btnClear.Text = "CLEAR";
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(27, 99);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 13);
+            this.label4.TabIndex = 40;
+            this.label4.Text = "Folder Input";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(313, 452);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 13);
+            this.label3.TabIndex = 45;
+            this.label3.Text = "CREDIT : @THUG";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // guna2ControlBox1
+            // 
+            this.guna2ControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2ControlBox1.FillColor = System.Drawing.Color.Black;
+            this.guna2ControlBox1.IconColor = System.Drawing.Color.White;
+            this.guna2ControlBox1.Location = new System.Drawing.Point(371, 3);
+            this.guna2ControlBox1.Name = "guna2ControlBox1";
+            this.guna2ControlBox1.Size = new System.Drawing.Size(33, 19);
+            this.guna2ControlBox1.TabIndex = 47;
+            // 
+            // guna2ControlBox2
+            // 
+            this.guna2ControlBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2ControlBox2.ControlBoxType = Guna.UI2.WinForms.Enums.ControlBoxType.MinimizeBox;
+            this.guna2ControlBox2.FillColor = System.Drawing.Color.Black;
+            this.guna2ControlBox2.IconColor = System.Drawing.Color.White;
+            this.guna2ControlBox2.Location = new System.Drawing.Point(332, 3);
+            this.guna2ControlBox2.Name = "guna2ControlBox2";
+            this.guna2ControlBox2.Size = new System.Drawing.Size(33, 19);
+            this.guna2ControlBox2.TabIndex = 48;
+            // 
+            // guna2ContainerControl1
+            // 
+            this.guna2ContainerControl1.Controls.Add(this.guna2ControlBox1);
+            this.guna2ContainerControl1.Controls.Add(this.guna2ControlBox2);
+            this.guna2ContainerControl1.FillColor = System.Drawing.Color.LightGray;
+            this.guna2ContainerControl1.Location = new System.Drawing.Point(12, 7);
+            this.guna2ContainerControl1.Name = "guna2ContainerControl1";
+            this.guna2ContainerControl1.Size = new System.Drawing.Size(404, 26);
+            this.guna2ContainerControl1.TabIndex = 49;
+            this.guna2ContainerControl1.Text = "guna2ContainerControl1";
+            // 
+            // Form2
+            // 
+            this.ClientSize = new System.Drawing.Size(428, 474);
+            this.Controls.Add(this.guna2ContainerControl1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnResetGuest);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtInputDir);
+            this.Controls.Add(this.txtOutputDir);
+            this.Controls.Add(this.btnSelectInputDir);
+            this.Controls.Add(this.btnSelectOutputDir);
+            this.Controls.Add(this.btnDump);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.statusTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form2";
+            this.ShowIcon = false;
+            this.Text = "GuestExtractor";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.guna2ContainerControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.TextBox txtInputDir;
+        private System.Windows.Forms.TextBox txtOutputDir;
+        private System.Windows.Forms.Button btnSelectInputDir;
+        private System.Windows.Forms.Button btnSelectOutputDir;
+        private System.Windows.Forms.Button btnDump;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.TextBox statusTextBox;
+        private Guna.UI2.WinForms.Guna2AnimateWindow guna2AnimateWindow1;
+        private Guna.UI2.WinForms.Guna2BorderlessForm guna2BorderlessForm1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnResetGuest;
+        private Guna.UI2.WinForms.Guna2AnimateWindow guna2AnimateWindow2;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private Guna.UI2.WinForms.Guna2ContainerControl guna2ContainerControl1;
+        private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox1;
+        private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox2;
+    }
+}
---
+++ Form/Form2.cs
@@ -0,0 +1,315 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using Aimguard;
+using Newtonsoft.Json;
+using System.Runtime.InteropServices;
+using static CuoreUI.DeviceInfo;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Linq;
+
+
+namespace GuestExtractor
+{
+    public partial class Form2 : Form
+    {
+        private string[] TaskName = { "HD-Player" };
+        private nexx32 nexx = new nexx32();
+
+        public Form2()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true;
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            UpdateStatus("Welcome to GuestExtractor.");
+        }
+
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        [DllImport("kernel32.dll")]
+        private static extern bool AllocConsole();
+
+        [DllImport("user32.dll")]
+        public static extern uint SetWindowDisplayAffinity(IntPtr hwnd, uint dwAffinity);
+
+        private string[] scanPatterns = new string[] {
+            "40 cd cc 8c 3f 8f c2 f5 3c cd cc CC 3D 07 00 00 00 00 00 00 00 00 00 00 00 00 00 f0 41 00 00 48 42 00 00 00 3f 33 33 13 40 00 00 b0 3f 00 00 80 3F 01 00 00",
+        "10 4C 2D E9 08 B0 8D E2 0C 01 9F E5 00 00 8F E0 00 00 D0 E5 00 00 50 E3 06 00 00 1A FC 00 9F E5"};
+
+
+
+        private string[] replacePatterns = new string[] { "40 cd cc 8C 3F 8f c2 F5 3C cd cc CC 3D 07 00 00 00 00 00 ff ff 00 00 00 00 00 00 f0 41 00 00 48 42 00 00 00 3f 33 33 13 40 00 00 b0 3f 00 00 80 3f 01 00 00",
+        "01 00 A0 E3 1E FF 2F E1 0C 01 9F E5 00 00 8F E0 00 00 D0 E5 00 00 50 E3 06 00 00 1A FC 00 9F E5"};
+
+        private async Task EnableFunction(string scanPattern, string replacePattern, string nameFonction)
+        {
+            await ToggleBytes(scanPattern, replacePattern, nameFonction, true);
+        }
+
+        private async Task DisableFunction(string scanPattern, string replacePattern, string nameFonction)
+        {
+            await ToggleBytes(scanPattern, replacePattern, nameFonction, false);
+        }
+
+        private async Task ToggleBytes(string scanPattern, string replacePattern, string nameFonction, bool activate)
+        {
+            bool success = nexx.getTask(TaskName);
+            if (!success)
+            {
+                UpdateStatus("Process not found.");
+                return;
+            }
+
+            try
+            {
+                await Task.Run(async () =>
+                {
+                    // Un message unique indiquant le démarrage de l'opération
+                    UpdateStatus($"{nameFonction}: {(activate ? "Activating" : "Deactivating")}...");
+
+                    IEnumerable<long> result = await nexx.Trace(activate ? scanPattern : replacePattern);
+
+                    if (result == null || !result.Any())
+                    {
+                        UpdateStatus($"{nameFonction}: No matching pattern found. Operation aborted.");
+                        return;
+                    }
+
+                    foreach (long id in result)
+                    {
+                        bool setSuccess = nexx.SetBytes(id, activate ? replacePattern : scanPattern);
+                        if (!setSuccess)
+                        {
+                            MessageBox.Show($"Failed to update memory at ID: {id}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            UpdateStatus($"{nameFonction}: Memory update failed at ID {id}.");
+                            return;
+                        }
+                    }
+
+                    UpdateStatus($"{nameFonction}: {(activate ? "Activated" : "Deactivated")}");
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Exception", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                UpdateStatus($"{nameFonction}: Error - {ex.Message}");
+            }
+        }
+
+        // Bouton de sécurisation (ex.: Anticheat Bypass)
+        private void securiteButton_Click(object sender, EventArgs e)
+        {
+            _ = EnableFunction(replacePatterns[0], scanPatterns[0], "Anticheat Bypass");
+        }
+
+        // Bouton de réinitialisation des guest (Guest Reset)
+        // Ici, nous distinguons deux opérations si deux ensembles de patterns sont définis.
+        private void btnResetGuest_Click(object sender, EventArgs e)
+        {
+            // Vérifie que les arrays contiennent au moins deux éléments pour éviter les erreurs
+            if (scanPatterns.Length >= 2 && replacePatterns.Length >= 2)
+            {
+                // Première étape de reset
+                _ = EnableFunction(scanPatterns[0], replacePatterns[0], "Guest Reset (Step 1)");
+                // Seconde étape de reset
+                _ = EnableFunction(scanPatterns[1], replacePatterns[1], "Guest Reset (Step 2)");
+            }
+            else
+            {
+                UpdateStatus("Guest Reset: Patterns not properly defined.");
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void btnSelectInputDir_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                txtInputDir.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void btnSelectOutputDir_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                txtOutputDir.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void btnDump_Click(object sender, EventArgs e)
+        {
+            string inputDir = txtInputDir.Text;
+            string outputDir = txtOutputDir.Text;
+            string outputFile = Path.Combine(outputDir, "guest_accounts.json");
+            string errorFile = Path.Combine(outputDir, "error_report.txt");
+
+            List<GuestAccountInfo> guestData = new List<GuestAccountInfo>();
+            HashSet<string> seenAccounts = new HashSet<string>();
+            List<string> errorReports = new List<string>();
+
+            if (!Directory.Exists(inputDir) || !Directory.Exists(outputDir))
+            {
+                UpdateStatus("Input/output directory does not exist.");
+                return;
+            }
+
+            UpdateStatus($"Starting scan in: {inputDir}");
+
+            int folderCount = 0;
+            try
+            {
+                folderCount = Directory.GetDirectories(inputDir).Length;
+            }
+            catch (Exception ex)
+            {
+                UpdateStatus($"Error: {ex.Message}");
+                return;
+            }
+
+            for (int i = 1; i <= folderCount; i++)
+            {
+                string dirPath = Path.Combine(inputDir, i.ToString());
+                string datFilePath = Path.Combine(dirPath, "guest100067.dat");
+                UpdateStatus($"Folder {i}: {dirPath}");
+
+                if (!Directory.Exists(dirPath) || !File.Exists(datFilePath))
+                {
+                    errorReports.Add($"Folder {i}: Missing file");
+                    continue;
+                }
+
+                GuestAccountInfo guest = ExtractGuestInfo(datFilePath, errorReports, i);
+                if (guest != null)
+                {
+                    string key = $"{guest.uid}-{guest.password}";
+                    if (!seenAccounts.Contains(key))
+                    {
+                        seenAccounts.Add(key);
+                        guestData.Add(guest);
+                    }
+                }
+            }
+
+            try
+            {
+                File.WriteAllText(outputFile, JsonConvert.SerializeObject(guestData, Formatting.Indented), Encoding.UTF8);
+                UpdateStatus($"Data saved to: {outputFile}");
+            }
+            catch (Exception ex)
+            {
+                UpdateStatus($"JSON write error: {ex.Message}");
+            }
+
+            try
+            {
+                File.WriteAllLines(errorFile, errorReports, Encoding.UTF8);
+                UpdateStatus($"Error report saved to: {errorFile}");
+            }
+            catch (Exception ex)
+            {
+                UpdateStatus($"Error report write failed: {ex.Message}");
+            }
+
+            UpdateStatus($"Scan complete. {guestData.Count} accounts found.");
+        }
+
+        private GuestAccountInfo ExtractGuestInfo(string filePath, List<string> errors, int index)
+        {
+            try
+            {
+                string content = File.ReadAllText(filePath).Trim();
+                if (string.IsNullOrEmpty(content)) return null;
+
+                var uid = Regex.Match(content, @"""com\.garena\.msdk\.guest_uid""\s*:\s*""(\d+)""");
+                var pw = Regex.Match(content, @"""com\.garena\.msdk\.guest_password""\s*:\s*""([A-F0-9]+)""");
+
+                if (uid.Success && pw.Success)
+                {
+                    return new GuestAccountInfo { uid = uid.Groups[1].Value, password = pw.Groups[1].Value };
+                }
+                errors.Add($"Folder {index}: extraction failed");
+            }
+            catch (Exception ex)
+            {
+                errors.Add($"Folder {index}: read error - {ex.Message}");
+            }
+            return null;
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtInputDir.Text = "";
+            txtOutputDir.Text = "";
+            statusTextBox.Clear();
+            UpdateStatus("Reset.");
+        }
+
+        private void UpdateStatus(string msg)
+        {
+            statusTextBox.AppendText($"{msg}{Environment.NewLine}");
+        }
+
+        private class GuestAccountInfo
+        {
+            public string uid { get; set; }
+            public string password { get; set; }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            statusTextBox.Clear();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = "https://t.me/nopethug", // Remplace par l'URL de ton choix
+                UseShellExecute = true
+            });
+        }
+
+        private void statusTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
